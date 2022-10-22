da364bf448ec9349d1109b89607bd63578f280ed
luvlore <brunaluchini22@gmail.com>
2022-10-22 14:38:47 -0300

    feat: add create user

--- src/controllers/User.py
+++ src/controllers/User.py
@@ -24,53 +24,76 @@ def find_by_name(name):
         print(formatter(user))
 
 def create():
-  new_user = {}
+  new_user = {'name': '', 'gender': '', 'email': '', 'phone': '', 'cpf': '', 'birthdate': ''}
   
-  print('CRIAÇÃO DE NOVO USUÁRIO')
+  is_create = False
   
-  while True:
-    try:
-      new_user['name'] = input('Digite seu primeiro nome: ').strip().capitalize()
+  while not is_create:
+    print('CRIAÇÃO DE NOVO USUÁRIO')
+    
+    while True:
+      try:
+        new_user['name'] = input('Digite seu primeiro nome: ').strip().capitalize()
+        
+        if not new_user['name'].isalpha():
+          raise Exception('Formato de nome incorreto')
+        
+        break
+      except Exception as err:
+        print(err)
+    
+    while True:
+      try:
+        for pos, gender in enumerate(genders): 
+          print(f'{pos + 1:02}: {gender}')
+        
+        escolha = input('Escolha seu gênero: ') 
+        
+        if not escolha.isdigit() or int(escolha) not in list(range(1, len(genders) + 1)):
+          raise ValueError('Digite um valor válido')
+        
+        new_user['gender'] = genders[int(escolha) - 1]
+        break
+      except ValueError as err:
+        print(err)
+        
+    while True:
+      try:
+        new_user['email'] = input('Digite seu email: ')
+        
+        if not validate_email(new_user['email']):
+          raise Exception('Digite um email válido')
+        
+        break
+      except Exception as err:
+        print(err)
+    
+    while True:
+      try:  
+        phone = input('Digite seu número de telefone: ')
+        new_user['phone'] = validate_phone(phone)
+        break
+      except Exception:
+        print('Formato de telefone inválido!')
       
-      if not new_user['name'].isalpha():
-        raise Exception('Formato de nome incorreto')
+    new_user['cpf']       = input('Digite seu CPF: ')
+    new_user['birthdate'] = input('Digite sua data de nascimento: ')
+    
+    print('\n', formatter(new_user))
+    
+    resp = ' '
+    
+    while resp not in 'SN':
+      resp = input('As informações conferem? ').strip().upper()[0]
       
-      break
-    except Exception as err:
-      print(err)
-  
-  while True:
-    try:
-      for pos, gender in enumerate(genders): 
-        print(f'{pos + 1:02}: {gender}')
+      if resp == 'S':
+        with open('src/database/users.csv', 'a') as file:
+          fieldnames = new_user.keys()
+          writer = csv.DictWriter(file, fieldnames)
+          
+          writer.writerow(new_user)
+          
+          pass
+        
+        is_create = True
       
-      escolha = input('Escolha seu gênero: ') 
-      
-      if not escolha.isdigit() or int(escolha) not in list(range(1, len(genders) + 1)):
-        raise ValueError('Digite um valor válido')
-      
-      new_user['gender'] = genders[int(escolha) - 1]
-      break
-    except ValueError as err:
-      print(err)
-      
-  while True:
-    try:
-      new_user['email'] = input('Digite seu email: ')
-      
-      if not validate_email(new_user['email']):
-        raise Exception('Digite um email válido')
-      
-      break
-    except Exception as err:
-      print(err)
-  
-  while True:
-    try:  
-      phone = input('Digite seu número de telefone: ')
-      new_user['phone'] = validate_phone(phone)
-      break
-    except Exception:
-      print('Formato de telefone inválido!')
-    # new_user['cpf']       = input('Digite seu CPF: ')
-    # new_user['birthdate'] = input('Digite sua data de nascimento: ')

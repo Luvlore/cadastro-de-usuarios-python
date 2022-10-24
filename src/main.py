4370eac56e201f0ba09fbf26b9e4c2c129842d9f
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-10-24 16:24:54 -0300

    main structure in place
    built a simple menu, it works. now I can create the functions,
    and later I can come back to this and refactor it.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 '''
 menu do sistema
 '''
-from utils import print_users
 
 n1 = '[1]Imprima a lista de usuários\n'
 n2 = '[2]Busque um usuário pelo nome\n'
@@ -10,10 +9,48 @@ n4 = '[4]Remova um usuário\n'
 n5 = '[5]Atualize um usuário\n'
 bonus = '[6]Estátisticas do sistema\n'
 quit = '[7]Encerrar o sistema\n'
-menu = f'Digite o número da ação a ser realizada:\n{n1}{n2}{n3}{n4}{n5}{bonus}{sair}'
+menu = f'Digite o número da ação a ser realizada:\n{n1}{n2}{n3}{n4}{n5}{bonus}{quit}'
 
 action = input(menu)
 
+data = 'fake'
+
 while action != '7':
     if action == '1':
-        print(print_users(data))
+        # print_users(data)
+        print('users data\n')
+        action = input(menu)
+    elif action == '2':
+        name = input('Digite o nome do usuario que deseja buscar: ')
+        # find_user(data, name)
+        print('find user\n')
+        action = input(menu)
+    elif action == '3':
+        print('Dados do novo usuário:\n')
+        name = input('nome: ')
+        gender = input('genero: ')
+        email = input('email: ')
+        phone = input('phone: ')
+        cpf = input('cpf: ')
+        birth = input('data de nascimento: ')
+        # user, message = add_user(data, name, gender, email, phone, cpf, birth)
+        print('new user\n')
+        action = input(menu)
+    elif action == '4':
+        name = input('Digite o nome do usuario que deseja remover: ')
+        # user, message = remove_user(data, name)
+        print('remove user\n')
+        action = input(menu)
+    elif action == '5':
+        name = input('Digite o nome do usuario que deseja atualizar: ')
+        print('Digite que item deverá ser atualizado:\n')
+        key_to_updated = input(
+            'nome | gênero | email | telefone | cpf | nascimento: ')
+        updated_value = input('\ndigite o novo valor: ')
+        # user, message = remove_user(data, name, key_to_updated, updated_value)
+        print('updated user\n')
+        action = input(menu)
+    elif action == '6':
+        # system_statistics(data)
+        print('statistics\n')
+        action = input(menu)

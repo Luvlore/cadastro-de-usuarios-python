41962a683a8b49653c7d4cff19cd2f44459e0db6
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-10-24 15:56:15 -0300

    beginning to build structure

---
+++ src/utils.py
@@ -0,0 +1,35 @@
+'''
+functions in use by the system
+'''
+
+
+def print_users(data):
+    '''print user list'''
+    return 'user list'
+
+
+def find_user(data, name):
+    '''finds by name and returns user data'''
+    return 'user data'
+
+
+def add_user(data, name, gender, email, phone, cpf, birth):
+    '''adds a new user'''
+    return 'the new user, sucess/fail message'
+
+
+def remove_user(data, name):
+    '''find by name and removes user'''
+    # could find by other keys
+    return 'the new user, sucess/fail message'
+
+
+def atualiza_user(data, name, dado_a_atualizar, valor_atualizado):
+    '''find by name and updates user'''
+    # could find by other keys
+    return 'the updated user(before/after), sucess/fail message'
+
+
+def system_statistics(data):
+    '''prints a formatted text of the system's statistics'''
+    return 'statistics'
